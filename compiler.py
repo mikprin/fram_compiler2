28161a8f9d1ba01d1f1cbe5cc8dfc3340fbd57c6
mikprin <miksolo@yandex.ru>
2020-11-25 14:06:32 +0300

    class

--- compiler.py
+++ compiler.py
@@ -1,9 +1,33 @@
 import argparse
+import time
 from config_reader import Config
 
-parser = argparse.ArgumentParser(description="Pass memory config")
-parser.add_argument('configuration', metavar='C', type=str, help="configuration file name")
+if __name__ ==  '__main__':
+    #Code execution in case of direct call
 
-args = parser.parse_args()
-config = Config(args.configuration)
-arch = config.load_arch()
+
+    start_time = time.perf_counter()
+    # Init parser
+    parser = argparse.ArgumentParser(description="Pass memory config")
+    parser.add_argument('configuration', metavar='C', type=str, help="configuration file name")
+    parser.add_argument('-v' , '--verbose' , help = "Set debug level and execute (0 for almost no debug , 5 for full debug)", type = int , default = self.Config.debug_level )
+
+    args = parser.parse_args()
+
+    config = Config(args.configuration)
+
+	if args.verbose != self.Config.debug_level:
+		config = args.verbose
+
+    arch = config.load_arch()
+
+
+    end_time = time.perf_counter()
+    compilation_time = end_time - start_time
+
+
+class Fram_Compiler:
+    """docstring for Fram_Compiler."""
+    def __init__(config, *args , **kwargs):
+        self.config = config
+        self.args = args
